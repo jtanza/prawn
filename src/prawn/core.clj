b8ca56bf6e85778ddcfe4370919b5048f5d91784
jtanza <tanzajohn@gmail.com>
2020-11-11 17:03:06 -0500

    wip flush out king movements

--- src/prawn/core.clj
+++ src/prawn/core.clj
@@ -1,20 +1,52 @@
 (ns prawn.core)
 
-(def a-file-mask (unchecked-long 0xfefefefefefefefe))
-(def h-file-mask (unchecked-long 0x7f7f7f7f7f7f7f7f))
+(def not-a-file (unchecked-long 0xfefefefefefefefe))
+(def not-h-file (unchecked-long 0x7f7f7f7f7f7f7f7f))
+
+(def square-map
+  {:a1 (bit-set 0 7)
+   :b1 (bit-set 0 6)
+   :c1 (bit-set 0 5)
+   :d1 (bit-set 0 4)
+   :e1 (bit-set 0 3)
+   :f1 (bit-set 0 2)
+   :g1 (bit-set 0 1)
+   :h1 0})
+
+(defn south
+  "Shift square x bits right. If no x is
+  provided shifts eight bits, "
+  ([square]
+   (south square 1))
+  ([square n]
+   (bit-shift-right square (* n 8))))
+
+(defn north
+  ([square]
+   (north square 1))
+  ([square n]
+   (bit-shift-left square (* n 8))))
+
+(defn west
+  ([square]
+   (west square 1))
+  ([square n]
+   (bit-and (bit-shift-right square n) not-h-file)))
+
+(defn east
+  ([square]
+   (east square 1))
+  ([square n]
+   (bit-and (bit-shift-left square n) not-a-file)))
+
+(defn king
+  [square]
+  (let [attacks (bit-or (east square) (west square) square)]
+    (bit-or (north attacks) (south attacks) attacks)))
+
 
 (def attack-map
   {:king king})
-
-(def piece-movement
-  {:king {:north 8
-          :south -8
-          :east 1
-          :west -1
-          :no-west 7
-          :no-east 9
-          :so-west -9
-          :so-east -7}})
 
 (def direction-operations
   {:east bit-shift-left})
@@ -26,25 +58,17 @@
   (into {} (map (fn [square] [square ((piece attack-map) square)])
                 (map #(bit-set 0 %) (range 0 64)))))
 
-(defn king
-  [square]
-  (bit-or (bit-or
-           (bit-and (bit-shift-left square 1) a-file-mask)
-           (bit-and (bit-shift-right square 1) a-file-mask))
-          (bit-or
-           (bit-and (bit-shift-left square 8) a-file-mask)
-           (bit-and (bit-shift-right square 8) a-file-mask))))
-
-(defn print-board
-  "Pretty prints a bitboard into an 8x8 chessboard"
-  [bitboard]
-  (clojure.string/join "\n" (map (partial apply str) (partition-all 8 (dec->bin bitboard)))))
-
 (defn dec->bin
   "Returns as a string the binary representation of a decimal
   value x, left pads to a full 64 bit value as necessary"
   [x]
   (let [bin (Long/toBinaryString x)]
     (str (apply str (repeat (- 64 (count bin)) "0")) bin)))
+
+(defn print-board
+  "Pretty prints a bitboard into an 8x8 chessboard"
+  [bitboard]
+  (clojure.string/join "\n" (map (partial apply str) (partition-all 8 (reverse (dec->bin bitboard))))))
+
 
 
--- test/prawn/core_test.clj
+++ test/prawn/core_test.clj
@@ -4,4 +4,4 @@
 
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 921102 (king (bit-set 0 10))))))
