41c4d73452450de7ff534527d89c14c2ec3741c0
jtanza <tanzajohn@gmail.com>
2020-11-14 15:17:47 -0500

    adds piece to bitboard mapping

--- src/prawn/core.clj
+++ src/prawn/core.clj
@@ -3,23 +3,31 @@
 (def not-a-file (unchecked-long 0xfefefefefefefefe))
 (def not-h-file (unchecked-long 0x7f7f7f7f7f7f7f7f))
 
-(def square-map
-  {:a1 (bit-set 0 7)
-   :b1 (bit-set 0 6)
-   :c1 (bit-set 0 5)
-   :d1 (bit-set 0 4)
-   :e1 (bit-set 0 3)
-   :f1 (bit-set 0 2)
-   :g1 (bit-set 0 1)
-   :h1 0})
+(def board-steps
+  "Represents "
+  (map (fn [a]
+         (for [x (range 0 64 8)
+               y [a]]
+           (- y x))) (range 56 64)))
+
+(def pieces
+  "A list of lists containing all 64 pieces of a chessboard (<file><rank>)
+  as keywords, e.g. '((:a1 :a2 ...) ... (:h1 ...)"
+  (map (fn [file]
+         (for [rank (range 1 9)]
+           (keyword (str file rank)))) (map #(char %) (range 97 105))))
+
+(def board-map
+  "Mapping from piece to bit position on a LERF based bitboard"
+  (into {} (map (fn [a b] (zipmap a b)) pieces board-steps)))
 
 (defn south
   "Shift square x bits right. If no x is
   provided shifts eight bits, "
   ([square]
    (south square 1))
-  ([square n]
-   (bit-shift-right square (* n 8))))
+  ([square x]
+   (bit-shift-right square (* x 8))))
 
 (defn north
   ([square]
@@ -39,17 +47,14 @@
   ([square n]
    (bit-and (bit-shift-left square n) not-a-file)))
 
+;; TODO can these just take piece keywords?
 (defn king
   [square]
   (let [attacks (bit-or (east square) (west square) square)]
     (bit-or (north attacks) (south attacks) attacks)))
 
-
 (def attack-map
   {:king king})
-
-(def direction-operations
-  {:east bit-shift-left})
 
 ;; {0 bitboard 1 bitboard}
 (defn gen-attack-moves
@@ -58,6 +63,7 @@
   (into {} (map (fn [square] [square ((piece attack-map) square)])
                 (map #(bit-set 0 %) (range 0 64)))))
 
+
 (defn dec->bin
   "Returns as a string the binary representation of a decimal
   value x, left pads to a full 64 bit value as necessary"
@@ -65,10 +71,12 @@
   (let [bin (Long/toBinaryString x)]
     (str (apply str (repeat (- 64 (count bin)) "0")) bin)))
 
-(defn print-board
+(defn pr-board
   "Pretty prints a bitboard into an 8x8 chessboard"
   [bitboard]
   (clojure.string/join "\n" (map (partial apply str) (partition-all 8 (reverse (dec->bin bitboard))))))
 
 
+;; this should work
+;; (print (pr-board (get (gen-attack-moves :king) (:e4 board-map))))
 
