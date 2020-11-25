9df0326b82af3dba05a0d4b2bb66298ea5c6cbc5
jtanza <tanzajohn@gmail.com>
2020-11-25 14:57:07 -0500

    adds knight movemnets

--- src/prawn/core.clj
+++ src/prawn/core.clj
@@ -3,8 +3,10 @@
 
 (def not-a-file (unchecked-long 0xfefefefefefefefe))
 (def not-h-file (unchecked-long 0x7f7f7f7f7f7f7f7f))
+(def not-ab-file (unchecked-long 0xfcfcfcfcfcfcfcfc))
+(def not-gh-file (unchecked-long 0x3f3f3f3f3f3f3f3f))
 
-(def board-steps
+(def file-steps
   "Represents the ordered transitions (as bitboards) along the
   files of a chessboard, i.e. from a1->a2->a3 etc"
   (map (fn [a]
@@ -12,24 +14,24 @@
                 y [a]]
             (bit-set 0 (- y x))))) (range 56 64)))
 
-(def ranked-squares
-  "An ordered list of lists containing all 8 ranks of a chessboard 
-  as keywords `(<file><rank>)`, e.g. `((:a1 :a2 ...) ... (:h1 ...)`"
+(def files
+  "An ordered list of lists containing all 8 files of a chessboard
+  e.g. `((:a1 :a2 ...) ... (:h1 ...)`"
   (map (fn [file]
          (for [rank (range 1 9)]
            (keyword (str file rank)))) (map #(char %) (range 97 105))))
 
 (def squares
-  "All 64 squares of a chessboard as keywords e.g. `(:a1 :a2 ... :h8)`"
-  (flatten ranked-squares))
+  "All 64 squares of a chessboard e.g. `(:a1 :a2 ... :h8)`"
+  (flatten files))
 
-(def board-map
+(def bitboard-map
   "Mapping from square to bit position on a LERF based bitboard."
-  (into {} (map (fn [a b] (zipmap a b)) ranked-squares board-steps)))
+  (into {} (map (fn [a b] (zipmap a b)) files file-steps)))
 
-(def inverted-board-map
+(def inverted-bitboard-map
   "Mapping from bit position to square on a LERF based bitboard"
-  (set/map-invert board-map))
+  (set/map-invert bitboard-map))
 
 (defn north
   ([square]
@@ -37,13 +39,43 @@
   ([square n]
    (bit-shift-left square (* n 8))))
 
+(defn north-north-east
+  [square]
+  (bit-and (bit-shift-left square 17) not-a-file))
+
+(defn north-east-east
+  [square]
+  (bit-and (bit-shift-left square 10) not-ab-file))
+
+(defn north-north-west
+  [square]
+  (bit-and (bit-shift-left square 15) not-h-file))
+
+(defn north-west-west
+  [square]
+  (bit-and (bit-shift-left square 6) not-gh-file))
+
 (defn south
-  "Shift square x bits right. If no x is
-  provided shifts eight bits, "
   ([square]
    (south square 1))
   ([square x]
    (bit-shift-right square (* x 8))))
+
+(defn south-east-east
+  [square]
+  (bit-and (bit-shift-right square 6) not-ab-file))
+
+(defn south-south-east
+  [square]
+  (bit-and (bit-shift-right square 15) not-a-file))
+
+(defn south-west-west
+  [square]
+  (bit-and (bit-shift-right square 10) not-gh-file))
+
+(defn south-south-west
+  [square]
+  (bit-and (bit-shift-right square 17) not-h-file))
 
 (defn west
   ([square]
@@ -57,20 +89,28 @@
   ([square n]
    (bit-and (bit-shift-left square n) not-a-file)))
 
-;; TODO can these just take piece keywords?
 (defn king
+  "Generates all legal king moves from the given square"
   [square]
   (let [attacks (bit-or (east square) (west square) square)]
     (bit-or (north attacks) (south attacks) attacks)))
 
+(defn knight
+  "Generates all legal knight moves from the given square"
+  [square]
+  (bit-or (north-north-east square) (north-north-west square) (north-east-east square)
+          (north-west-west square) (south-south-east square) (south-south-west square)
+          (south-west-west square) (south-east-east square)))
+
 (def attack-map
-  {:king king})
+  {:king king
+   :knight knight})
 
 (defn gen-attack-moves
   "Given a chess piece, generates all legal attack moves of that piece along every
   square of a chessboard as a map of bitboards."
   [piece]
-  (into {} (map (fn [square] [(inverted-board-map square) ((piece attack-map) square)])
+  (into {} (map (fn [square] [(inverted-bitboard-map square) ((piece attack-map) square)])
                 (map #(bit-set 0 %) (range 0 64)))))
 
 (defn dec->bin
@@ -83,5 +123,5 @@
 (defn pr-board
   "Pretty prints a bitboard into an 8x8 chessboard"
   [bitboard]
-  (clojure.string/join "\n" (map (partial apply str) (reverse (partition-all 8 (reverse (dec->bin bitboard)))))))
+  (print (clojure.string/join "\n" (map (partial apply str) (reverse (partition-all 8 (reverse (dec->bin bitboard))))))))
 
--- test/prawn/core_test.clj
+++ test/prawn/core_test.clj
@@ -3,16 +3,26 @@
             [prawn.core :refer :all]))
 
 (deftest lerf-board-mapping
-  (is (= 1 (:a1 board-map)))
-  (is (= 2 (:b1 board-map)))
-  (is (= Long/MIN_VALUE (:h8 board-map))))
+  (is (= 1 (:a1 bitboard-map)))
+  (is (= 2 (:b1 bitboard-map)))
+  (is (= Long/MIN_VALUE (:h8 bitboard-map))))
 
 (deftest rose-compass-move-generation
-  (is (= (:a2 board-map) (north (:a1 board-map))))
-  (is (= (:a3 board-map) (north (:a1 board-map) 2)))
-  (is (= (:e3 board-map) (south (:e4 board-map))))
-  (is (= (:e1 board-map) (south (:e4 board-map) 3)))
-  (is (= (:b2 board-map) (west (:c2 board-map))))
-  (is (= (:a2 board-map) (west (:c2 board-map) 2)))
-  (is (= (:h4 board-map) (east (:g4 board-map))))
-  (is (= (:e4 board-map) (east (:a4 board-map) 4))))
+  (is (= (:a2 bitboard-map) (north (:a1 bitboard-map))))
+  (is (= (:a3 bitboard-map) (north (:a1 bitboard-map) 2)))
+  (is (= (:e3 bitboard-map) (south (:e4 bitboard-map))))
+  (is (= (:e1 bitboard-map) (south (:e4 bitboard-map) 3)))
+  (is (= (:b2 bitboard-map) (west (:c2 bitboard-map))))
+  (is (= (:a2 bitboard-map) (west (:c2 bitboard-map) 2)))
+  (is (= (:h4 bitboard-map) (east (:g4 bitboard-map))))
+  (is (= (:e4 bitboard-map) (east (:a4 bitboard-map) 4))))
+
+(deftest king-movements
+  (let [moves (gen-attack-moves :king)]
+    (is (= 771 (:a1 moves)))
+    (is (= 241461362688 (:e4 moves)))))
+
+(deftest knight-movements
+  (let [moves (gen-attack-moves :knight)]
+    (is (= 132096 (:a1 moves)))
+    (is (= 44272527353856 (:e4 moves)))))
